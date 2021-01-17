073b031d62ad2f8e5cce16fcc0af1adcf3455711
AbinandaP <66347452+AbinandaP@users.noreply.github.com>
2021-01-17 12:34:43 +0530

    creating table maintable

---
+++ creationmaintable.py
@@ -0,0 +1,8 @@
+import sqlite3 as sq
+import datetime as dt
+
+conn=sq.connect('points.db')
+cursor=conn.cursor()
+cmd="create table maintable(DATE date,TIME time);"
+cursor.execute(cmd)
+conn.commit()
